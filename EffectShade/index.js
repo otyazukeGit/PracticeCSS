549a3f17d6ea533d25c65ebb6392e46ef514d4e1
uuser <otyazuke.gookle@gmail.com>
2020-10-23 15:58:21 +0900

    onload Fade-in opacity

---
+++ EffectShade/index.js
@@ -0,0 +1,9 @@
+// $(document).ready()
+// document.addEventListener("DOMContentLoaded", function () {
+
+window.addEventListener('load', (event) => {
+	let ele = document.querySelector("h1")
+	ele.classList.add('effect');
+	// ele.classList.remove('effect')
+
+})
